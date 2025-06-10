d7d55f49087a2f59c1e68dd40a26bc7654a15169
Brian Worthington <worthington.brian@realtyts.com>
2025-06-10 14:43:34 -0500

    Update wsgi.py to explicitly initialize app and handle port configuration

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from rt_search_flask import app
+import os
+from rt_search_flask import init_app
+
+app = init_app()
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
